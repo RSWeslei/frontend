8139909ceade867c95512102a3839c6e8880f400
RSWeslei <weslei67rosiak.com@gmail.com>
2022-07-19 23:05:00 -0300

    eu

--- pages/login.vue
+++ pages/login.vue
@@ -1,68 +1,81 @@
 <template>
   <v-container class="all">
-    <v-row justify="space-around">
+    <v-row justify="center">
       <v-card>
         <v-container class="login">
-          <v-container class="padding">
-            <v-form>
-              <v-text-field
-                height="50"
-                solo
-                :outlined="showOutline1"
-                color="#8257e5"
-                dense
-                background-color="#121214"
-                label="Email"
-                type="email"
-                placeholder="Email"
-                prepend-inner-icon="mdi-email"
-                @focusin="showOutline1 = !showOutline1"
-                @focusout="showOutline1 = !showOutline1"
-              />
-            </v-form>
-            <v-form>
-              <v-text-field
-                height="50"
-                solo
-                background-color="#121214"
-                :outlined="showOutline2"
-                color="#8257e5"
-                dense
-                label="Senha"
-                placeholder="Senha"
-                :type="showPassword ? 'text' : 'password'"
-                prepend-inner-icon="mdi-lock"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                @click:append="showPassword = !showPassword"
-                @focusin="showOutline2 = !showOutline2"
-                @focusout="showOutline2 = !showOutline2"
-              />
-            </v-form>
-          </v-container>
-          <v-container class="submitInfo padding">
-            <v-row>
-              <v-container>
-                <v-col class="forgetPassword">
-                  <a style="color: #8257e5; text-decoration: none" href="https://app.rocketseat.com.br/forgot">Esqueci minha senha</a>
-                </v-col>
-              </v-container>
-              <v-container class="loginButton">
-                <v-col>
-                  <v-btn
-                    width="352px"
-                    height="50"
-                    class="ma-2"
-                    :loading="loading"
-                    :disabled="loading"
-                    color="#41356b"
-                    @click="loader = 'loading'"
-                  >
-                    ENTRAR
-                  </v-btn>
-                </v-col>
-              </v-container>
-            </v-row>
-          </v-container>
+          <v-col>
+            <v-container class="padding">
+              <v-form>
+                <v-text-field
+                  height="50"
+                  solo
+                  :outlined="showOutline1"
+                  color="#8257e5"
+                  dense
+                  background-color="#121214"
+                  label="Email"
+                  type="email"
+                  placeholder="Email"
+                  prepend-inner-icon="mdi-email"
+                  @focusin="showOutline1 = !showOutline1"
+                  @focusout="showOutline1 = !showOutline1"
+                />
+              </v-form>
+              <v-form>
+                <v-text-field
+                  height="50"
+                  solo
+                  background-color="#121214"
+                  :outlined="showOutline2"
+                  color="#8257e5"
+                  dense
+                  label="Senha"
+                  placeholder="Senha"
+                  :type="showPassword ? 'text' : 'password'"
+                  prepend-inner-icon="mdi-lock"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="showPassword = !showPassword"
+                  @focusin="showOutline2 = !showOutline2"
+                  @focusout="showOutline2 = !showOutline2"
+                />
+              </v-form>
+            </v-container>
+          </v-col>
+          <v-col align="center">
+            <v-container class="submitInfo">
+              <v-row>
+                <v-container>
+                  <v-col class="forgetPassword">
+                    <a style="color: #8257e5; text-decoration: none" href="https://app.rocketseat.com.br/forgot">Esqueci minha senha</a>
+                  </v-col>
+                </v-container>
+                <v-container>
+                  <v-col class="loginButton">
+                    <v-btn
+                      width="352px"
+                      height="50"
+                      color="#41356b"
+                      depressed="true"
+                      disabled
+                    >
+                      ENTRAR
+                    </v-btn>
+                  </v-col>
+                </v-container>
+                <v-container>
+                  <v-col>
+                    <b style="padding-right: 6px" >Nao tem uma conta?</b>
+                    <a
+                      class="link"
+                      href="https://app.rocketseat.com.br/signup"
+                    >
+                      Registre-se
+                    </a>
+                  </v-col>
+                </v-container>
+              </v-row>
+            </v-container>
+          </v-col>
         </v-container>
       </v-card>
     </v-row>
@@ -76,7 +89,8 @@ export default {
     return {
       showPassword: false,
       showOutline1: false,
-      showOutline2: false
+      showOutline2: false,
+      mouseOver: false
     }
   }
 }
@@ -90,20 +104,35 @@ export default {
     padding-bottom: 50px;
     width: 480px;
     height: 476px;
+    /* background-color: brown; */
   }
   .padding {
-    padding-right: 50px;
-    padding-left: 50px;
+    padding-right: 40px;
+    padding-left: 40px;
+    /* background-color: chartreuse; */
   }
   .submitInfo {
     margin-top: -60px;
-    margin-left: -10px;
+    /* background-color: blue; */
   }
   .forgetPassword {
     font-size: 14px;
+    padding-right: 220px;
+    margin-top: -20px;
   }
   .loginButton {
-    text-align: center;
     margin-top: -30px;
+    width: fit-content;
+    /* background-color: #FF0000; */
+  }
+  .loginButton:hover {
+    cursor: not-allowed;
+  }
+  .link {
+    text-decoration: none;
+    color: #FF0000;
+  }
+  .link:hover {
+    color:rgb(130, 87, 229);
   }
 </style>
